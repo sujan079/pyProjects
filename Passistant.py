0e99d2d55e78540dbb919bb07beaa7924583a3f0
Suzan Adk <079bct086@ioepc.edu.np>
2024-01-02 16:33:25 +0545

    2nd commit

--- Passistant.py
+++ Passistant.py
@@ -1,4 +1,5 @@
-# make sure that you install the pyaudio and speech recognition package 
+# Make sure to install the required packages: pyaudio, speech_recognition, pyttsx3, pywhatkit, wikipedia, pyjokes
+
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
@@ -7,18 +8,18 @@ import wikipedia
 import pyjokes
 
 listener = sr.Recognizer()
-engine= pyttsx3.init()
+engine = pyttsx3.init()
 voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[1].id)#changing the voice to female voice
+engine.setProperty('voice', voices[1].id)  # Changing the voice to a female voice
+
 def talk(txt):
     engine.say(txt)
     engine.runAndWait()
 
-
 def take_command():
     try:
         with sr.Microphone() as source:
-            print("listening....")
+            print("Listening....")
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
@@ -30,40 +31,37 @@ def take_command():
 def run_alexa():
     command = take_command()
     if 'play' in command:
-        song=command.replace('play','')
-        talk("playing "+song)
-        talk("playing")
-        pywhatkit.playonyt(song)#plays the song form the youtube
+        song = command.replace('play', '')
+        talk("Playing " + song)
+        talk("Playing")
+        pywhatkit.playonyt(song)  # Plays the song from YouTube
     elif 'time' in command:
         time = datetime.datetime.now().strftime("%I:%M %p")
-        #formatting the time
-        talk('Current time is'+time)
+        talk('Current time is ' + time)
         print(time)
     elif 'who is' in command:
-        main_word = command.replace('who is','')
-        info = wikipedia.summary(main_word,1) #getting three line of the info form the wikipedia
+        main_word = command.replace('who is', '')
+        info = wikipedia.summary(main_word, 1)
         print(info)
         talk(info)
     elif 'what is' in command:
         main_word = command.replace('what is', '')
-        info = wikipedia.summary(main_word, 1)  # getting three line of the info form the wikipedia
+        info = wikipedia.summary(main_word, 1)
         print(info)
         talk(info)
     elif 'date' in command:
-        talk('sorry ,I am already occupied')
+        talk('Sorry, I am already occupied')
     elif 'are you single' in command:
-        talk('I can\'t be in relation with anyone except your wifi')
+        talk('I can\'t be in a relationship with anyone except your wifi')
     elif 'joke' in command:
         joke = pyjokes.get_joke()
         print(joke)
         talk(joke)
     elif 'quit the program' in command:
-        talk('goodbye')
+        talk('Goodbye')
+        exit()  # Exit the program
     else:
         talk('Please say the command again')
 
-
 while True:
     run_alexa()
-
-
